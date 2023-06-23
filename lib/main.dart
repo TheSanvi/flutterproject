0d8d9964f954fc19c58bc144cfc26a2a895b5881
TheSanvi <sanvivermavns@gmail.com>
2023-06-23 11:30:17 +0530

    main and cross axis

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
         Column(
 
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.center,
 
             children: [
             Text("4"),
