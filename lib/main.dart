3dc5739d74da3dae5864d6b3a4aab932752d94bc
TheSanvi <sanvivermavns@gmail.com>
2023-06-23 20:37:59 +0530

    quiz app created
    drawer and navigation used

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,108 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  static const appTitle = 'app : Many Quizzes';
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.cyan,
-            title: const Text("My App"),
-            centerTitle: true,
-          ),
-          body: Padding(
-            padding: const EdgeInsets.only(left: 50),
-            child: Container(
-              height: 300,
-              width: 500,
-              child: const Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('1'),
-                        Text('2'),
-                        Text('3'),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: appTitle,
+      home: MYQuiz(title: appTitle),
+    );
+  }
+}
 
-                      children: [
-                        Text('4'),
-                        Text('5'),
-                        Text('6'),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//*********************************************
+class MYQuiz extends StatefulWidget {
+  final String title;
+  const MYQuiz({super.key, required this.title});
 
-                      children: [
-                        Text('7'),
-                        Text('8'),
-                        Text('9'),
-                      ],
-                    ),
-                  ],
-                ),
+  @override
+  State<MYQuiz> createState() => _MYQuiz();
+}
+
+class _MYQuiz extends State<MYQuiz> {
+  String quizname = "The Quiz will appear here.";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 8, 0),
+              child: Text(widget.title),
+            ),
+            CircleAvatar(
+              radius: 25,
+              backgroundColor: Colors.yellow,
+              child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: ClipOval(child: Image.network(Utilities.imagepath)),
               ),
             ),
-          ),
-
-        ));
+          ],
+        ),
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: Text(quizname),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: const BoxDecoration(
+                color: Colors.deepPurple,
+              ),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: 56,
+                    backgroundColor: Colors.teal,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child:
+                      ClipOval(child: Image.network(Utilities.imagepath)),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Text('Available Quizzes'),
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              title: const Text('Quiz 1'),
+              onTap: () {
+                quizname = "Quiz 1";
+                setState(() {});
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text("Quiz 2"),
+              onTap: () {
+                quizname = "Quiz 2";
+                setState(() {});
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
+}
+
+class Utilities {
+  static String imagepath =
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjy2x0ON-9WhkLuW8TtHODEummjn9UEX7DLYX-mCQeTANCnLbJHwegljnZoKn3Zz2kFaQ5H_u_EqbbVzDE1wCtFKvUY9haHbZxPWgYCjlEeMMhzkJrP5dhnI-rEDcE950upo45KF_7DxxgO3615YSAGy2FmOuirMgvnUrHFHYo8VcLSaPhgcmgOZBGDNRrh/s320/IMG20230507181718.jpg";
 }
